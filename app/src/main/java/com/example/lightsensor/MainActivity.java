c77527f49501ab457cd700dcece417af910f9903
unknown <you@example.com>
2021-01-01 02:33:36 +0200

    Starter

---
+++ app/src/main/java/com/example/lightsensor/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.lightsensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity{
+    //Button btn;
+    EditText year;
+    Button btn;
+    TextView age;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        year =  findViewById(R.id.year);
+        btn = (Button) findViewById(R.id.btn);
+        age = findViewById(R.id.age);
+        // click --
+        // get Years from edit text --
+        // calculate
+        // display
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int years = Integer.parseInt(String.valueOf(year.getText()));
+                int myAge = 2021 - years;
+                age.setText(String.valueOf(myAge));
+            }
+        });
+
+
+        //btn = findViewById(R.id.btn);
+        /*btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(this,"U Click ME !!", Toast.LENGTH_LONG).show();
+            }
+        });
+        */
+    }
+
+
+}
